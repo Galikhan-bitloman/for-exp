1c4c282fe355f54b2b5e49a0a40cc0b76cc804ac
Galikhan-bitloman <galikhan.abdikarim@gmail.com>
2020-07-20 23:41:24 +0500

    new add

---
+++ task6_capitaliz_me_programmr.py
@@ -0,0 +1,7 @@
+from distlib.compat import raw_input
+
+a = raw_input("Enter a string:")
+
+if a:
+
+    print(a.title())
---
+++ task7_reverse_the_sentences_programmr.py
@@ -0,0 +1,10 @@
+from distlib.compat import raw_input
+
+inp = raw_input()
+for i in inp:
+    i.sorted(reverse = True)
+
+
+
+
+
